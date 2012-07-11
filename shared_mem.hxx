2bdd91f28780e0af08b0b2759fc3f66700d01726
Matt Renaud <mrenaud92@gmail.com>
2012-07-11 15:22:46 -0400

    Moved core functionality of shared memory to shared_memory_base<T,N>

--- shared_mem.hxx
+++ shared_mem.hxx
@@ -36,30 +36,36 @@ namespace posix {
 
 //=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 
+//m=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+template <typename T, unsigned Size>
+struct shared_memory;
 
-template <typename T, unsigned Size = 1>
-struct shared_memory
+
+//m=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+template <typename T, unsigned Size>
+struct shared_memory_base
 {
   using value_type = T;
-  using stripped_ptr_type = typename std::remove_pointer<value_type>::type;
   using flag_type = int;
+
+private:
+  friend class shared_memory<T,Size>;
+
   
-  shared_memory(int flags)
+protected:
+  shared_memory_base(int flags)
     : segment_id_(shmget(IPC_PRIVATE, sizeof(T)*Size, flags)),
       shared_mem_(static_cast<value_type*>(shmat(segment_id_, NULL, 0)))
   {
   }
 
-  shared_memory()
+  shared_memory_base()
     : segment_id_(-1), shared_mem_(nullptr)
   {
   }
 
-  shared_memory(shared_memory const& shm)
-    : segment_id_(shm.segment_id_), shared_mem_(shm.shared_mem_)
-  {
-  }
 
+public:  
   int segment_id() const
   {
     return segment_id_;
@@ -81,48 +87,72 @@ struct shared_memory
     shmctl(segment_id_, IPC_RMID, NULL);
   }
 
-  ~shared_memory()
+  ~shared_memory_base()
   {
     release();
   }
 
+
+protected:
+  int segment_id_;
+  value_type* shared_mem_;
+
+}; // struct shared_memory base
+
+
+
+//m=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+template <typename T, unsigned Size = 1>
+struct shared_memory : shared_memory_base<T,Size>
+{
+  using base_type = shared_memory_base<T,Size>;
+
+  using value_type = typename base_type::value_type;
+  using flag_type = typename base_type::flag_type;
+  
+  shared_memory(int flags)
+    : base_type(flags)
+  {
+  }
+
+  shared_memory()
+    : base_type()
+  {
+  }
+
   shared_memory& operator =(value_type const& val)
   {
-    *shared_mem_ = val;
+    *base_type::shared_mem_ = val;
     return *this;
   }
 
   // For char[]
   shared_memory& operator =(value_type const * const& str)
   {
-    sprintf(shared_mem_, str);
+    sprintf(base_type::shared_mem_, str);
     return *this;
   }
 
   value_type* operator&()
   {
-    return shared_mem_;
+    return base_type::shared_mem_;
   }
 
   std::ostream& print(std::ostream& os) const
   {
     if(Size > 1 && std::is_same<value_type,char>::value)
-      os << shared_mem_;
+      os << base_type::shared_mem_;
     else
-      os << *shared_mem_;
+      os << *base_type::shared_mem_;
 
     return os;
   }
 
   value_type& operator*()
   {
-    return *shared_mem_;
+    return *base_type::shared_mem_;
   }
      
-private:
-  int segment_id_;
-  value_type* shared_mem_;
-  
 }; // struct shared_memory
 
 
