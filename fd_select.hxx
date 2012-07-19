55019abebad91f029772e7f4d5dba42d074d4468
Matt Renaud <mrenaud92@gmail.com>
2012-07-19 13:40:12 -0400

    Added file descriptor select

---
+++ fd_select.hxx
@@ -0,0 +1,101 @@
+#ifndef MRR_POSIX_FD_SELECT_HXX_
+#define MRR_POSIX_FD_SELECT_HXX_
+
+#include <sys/select.h>
+#include <functional>
+#include <vector>
+
+//m=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+
+namespace mrr {
+namespace posix {
+
+//=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+
+struct fd_select
+{
+  using function_type = std::function<void(int)>;
+  
+  fd_select()
+  {
+    FD_ZERO(&readset);
+  }
+
+  int listen()
+  {
+    int retval;
+    
+    checkset = readset;
+    do
+    {
+      errno = 0;
+      retval = ::select(FD_SETSIZE, &checkset, NULL, NULL, NULL);
+    } while(retval == -1 && errno == EINTR);
+
+    if(retval == -1)
+      return -1;
+    
+    for(int fd : fds)
+      if(FD_ISSET(fd, &checkset))
+	return fd;
+  }
+
+  int listen_with_action()
+  {
+    int retval;
+
+    for(;;)
+    {
+      checkset = readset;
+      do
+      {
+	errno = 0;
+	retval = ::select(FD_SETSIZE, &checkset, NULL, NULL, NULL);
+      } while(retval == -1 && errno == EINTR);
+
+      if(retval == -1)
+	return -1;
+
+      std::size_t pos = 0;
+    
+      for(int fd : fds)
+      {
+      	if(FD_ISSET(fd, &checkset))
+      	  actions[pos](fd);
+      	++pos;
+      }
+    }
+
+  } // int listen_with_action()
+  
+
+  void watch(int fd)
+  {
+    fds.push_back(fd);
+    FD_SET(fd, &readset);
+  }
+
+  void watch(int fd, function_type&& f)
+  {
+    watch(fd);
+    actions.push_back(f);
+  }
+
+private:
+
+  fd_set readset, checkset;
+  std::vector<int> fds;
+  std::vector<function_type> actions;
+
+}; // struct fd_select
+
+
+
+//m=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+
+} // namespace posix
+} // namespace mrr
+
+//=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+
+#endif // #ifndef MRR_POSIX_FD_SELECT_HXX_
---
+++ test_code/test_select.cxx
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <fcntl.h>
+
+#include "../child_process.hxx"
+#include "../fdstream.hxx"
+#include "../pipe.hxx"
+#include "../fd_select.hxx"
+
+
+void print_number(int fd, int num)
+{
+  mrr::posix::fd_ostream write(fd);
+
+  sleep(num);
+  write << num << std::endl;
+}
+
+void read_numbers(int fd)
+{
+  int num;
+  std::cout << "Activity from " << fd << std::endl;
+  mrr::posix::fd_istream read(fd);
+  read >> num;
+  std::cout << "Num = " << num << std::endl;
+}
+
+int main()
+{
+  mrr::posix::pipe p1;
+  mrr::posix::pipe p2;
+
+  auto c1 = mrr::posix::make_child_process(print_number);
+  auto c2 = mrr::posix::make_child_process(print_number);
+
+  c1.fork(p1.write_end(), 1);
+  c1.wait(WNOHANG);
+
+  c2.fork(p2.write_end(), 2);
+  c2.wait(WNOHANG);
+
+  mrr::posix::fd_select p_select;
+
+  p_select.watch(p1.read_end(), read_numbers);
+  p_select.watch(p2.read_end(), read_numbers);
+  p_select.listen_with_action();
+}
