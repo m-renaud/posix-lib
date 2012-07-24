af52480fb57b2a613b7eee8abdb47428a451faac
Matt Renaud <mrenaud92@gmail.com>
2012-07-24 15:13:46 -0400

    Added signal handler

---
+++ signal_handler.hxx
@@ -0,0 +1,70 @@
+//m=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+//
+// Copyright (c) 2012, Matthew Renaud
+// All rights reserved.
+//
+// Licensed under the FreeBSD license. See LICENSE file for details.
+//
+//=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+
+#ifndef MRR_SIGNAL_HANDLER_HXX_
+#define MRR_SIGNAL_HONDLER_HXX_
+
+
+#include <functional>
+#include <unordered_map>
+
+#include <signal.h>
+
+
+//m=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+
+namespace mrr {
+namespace posix {
+
+//=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+
+
+//m=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+void global_signal_handling_function(int signo);
+
+
+//m=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+struct signal_handler
+{
+  void set_handle(int signo, std::function<void()> const& f)
+  {
+    struct sigaction act;
+    act.sa_handler = signal_handling_global_function;
+    sigaction(signo, &act, NULL);
+    handles[signo] = f;
+  }
+
+  std::unordered_map<int,std::function<void()> > handles;
+
+}; // struct signal_handler;
+
+
+
+//m=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+signal_handler signals;
+
+
+//m=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+void signal_handling_global_function(int signo)
+{
+  signals.handles[signo]();
+  struct sigaction act;
+  act.sa_handler = signal_handling_global_function;
+  sigaction(signo, &act, NULL);
+}
+
+
+//m=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+
+} // namespace posix
+} // namespace mrr
+
+//=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+
+#endif // #ifndef MRR_SIGNAL_HANDLER_HXX_
